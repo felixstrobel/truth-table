2b5b0771f64b8504e34d656bae2dc2343abd5376
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 00:03:34 +0200

    added todo

--- server/src/util/Lexer.ts
+++ server/src/util/Lexer.ts
@@ -19,6 +19,7 @@ export default class Lexer {
 		this.currentToken = this.input.charAt(this.currentIndex);
 	}
 
+	// TODO brackets
 	public nextToken(): { token: Token; symbol?: string } {
 		if (this.currentToken === '') return { token: Token.EOF };
 
