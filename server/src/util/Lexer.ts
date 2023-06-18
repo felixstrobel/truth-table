d66f4979048516caf8cce7ddd378607b39c48588
Felix Strobel <62568203+felixstrobel@users.noreply.github.com>
2023-06-18 23:03:09 +0200

    added Token and Lexer

--- server/src/util/Lexer.ts
+++ server/src/util/Lexer.ts
@@ -1,7 +1,16 @@
 export default class Lexer {
-	private input;
+	private input: string;
+	private currentIndex: number = 0;
 
 	constructor(input: string) {
-		this.input = input;
+		this.input = input.trim();
+	}
+
+	public nextToken(): Token {
+		let char = this.input.charAt(this.currentIndex);
+
+		if (char === '') return Token.EOF;
+
+		return Token.AND_OPERATOR;
 	}
 }
