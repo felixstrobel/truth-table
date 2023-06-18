1818d2bd1fba629b99bc39fe5e5be0a09343f77a
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 00:03:42 +0200

    added parse method

--- server/src/util/Parser.ts
+++ server/src/util/Parser.ts
@@ -1,11 +1,23 @@
 import Lexer from './Lexer';
+import Token from '../model/tokens/Token';
 
 export default class Parser {
 	private lexer: Lexer;
+	private variables: string[] = [];
 
 	constructor(lexer: Lexer) {
 		this.lexer = lexer;
 	}
 
-    
+	public parse() {
+		let token;
+		// TODO rewrite
+		while ((token = this.lexer.nextToken()).token !== Token.EOF) {
+			console.log(token);
+		}
+	}
 }
+
+// A|(B&C)|C
+// (A|(B&C))|C
+// (A|B)|C
