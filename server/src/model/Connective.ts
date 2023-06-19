e96bf20ba1032fe62d48c0db49a6ee3900ee5bd9
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 19:47:04 +0200

    added Connective

---
+++ server/src/model/Connective.ts
@@ -0,0 +1,22 @@
+import { EvalFunction } from '../types';
+
+export default class Connective {
+	private readonly officialSymbol: string;
+	private readonly otherSymbols: string[];
+	private readonly evalFunction: EvalFunction;
+
+	constructor(officialSymbol: string, evalFunction: EvalFunction, otherSymbols: string[] = []) {
+		this.officialSymbol = officialSymbol;
+		this.otherSymbols = otherSymbols;
+
+		this.evalFunction = evalFunction;
+	}
+
+	public getOfficialSymbol(): string {
+		return this.officialSymbol;
+	}
+
+	public getOtherSymbols(): string[] {
+		return this.otherSymbols;
+	}
+}
