8cf3a61595bce44930e8609a6d34436c40874ddd
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 22:40:05 +0200

    added doc

--- server/src/util/Parser.ts
+++ server/src/util/Parser.ts
@@ -1,21 +1,30 @@
-import Lexer from './Lexer';
-import Token from '../model/token/Token';
+import Lexer from "./Lexer";
+import Token from "../model/token/Token";
 
+/**
+ * TODO DOC
+ *
+ * @author Max Lohrmann <https://github.com/Max0440>
+ */
 export default class Parser {
 	private lexer: Lexer;
 	private variables: string[] = [];
 
+	/**
+	 * Constructs a {@link Parser} object.
+	 *
+	 * @param input The lexer used to generate the tokens.
+	 */
 	constructor(lexer: Lexer) {
 		this.lexer = lexer;
 	}
 
 	public parse() {
-		let token;
-		// new Connective('a', (a: boolean) => a);
-		// TODO rewrite
-		// while ((token = this.lexer.nextToken()).token !== Token.EOF) {
-		// 	console.log(token);
-		// }
+		const tokens = this.lexer.lex();
+
+		for (const token of tokens) {
+			// TODO handle token siht
+		}
 	}
 }
 
