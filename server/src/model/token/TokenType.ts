5e0c618a9208825025406523bd0d2d51b5e83829
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 22:04:37 +0200

    added doc

--- server/src/model/token/TokenType.ts
+++ server/src/model/token/TokenType.ts
@@ -1,11 +1,42 @@
+/**
+ * Enumeration of token types for the {@link Lexer}.
+ *
+ * @author Max Lohrmann <https://github.com/Max0440>
+ */
 enum TokenType {
+	/**
+	 * Represents an atom token (e.g. variable).
+	 */
 	ATOM,
+
+	/**
+	 * Represents a unary connective token (e.g. not).
+	 */
 	UNARY_CONNECTIVE,
+
+	/**
+	 * Represents a binary connective token (e.g. and).
+	 */
 	BINARY_CONNECTIVE,
+
+	/**
+	 * Represents an opening parenthesis token.
+	 */
 	PARENTHESIS_OPEN,
+
+	/**
+	 * Represents an closing parenthesis token.
+	 */
 	PARENTHESIS_CLOSE,
-	TERM,
+
+	/**
+	 * Represents an unknown token.
+	 */
 	UNKNOWN,
+
+	/**
+	 * Represents the end-of-file token.
+	 */
 	EOF,
 }
 
