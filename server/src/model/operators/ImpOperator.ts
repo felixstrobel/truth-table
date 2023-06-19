5791dc68cf8df518ddd65d1ad925bb17883f5100
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-19 19:47:59 +0200

    deleted operators (not needed anymore)

--- server/src/model/operators/ImpOperator.ts
+++
@@ -1,7 +0,0 @@
-import Term from "../Term";
-
-export default class ImpOperator extends Term {
-	public eval(variableAssignment: Map<string, boolean>): boolean {
-		return !this.leftChild!.eval(variableAssignment) || this.rightChild!.eval(variableAssignment);
-	}
-}
