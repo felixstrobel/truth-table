88d0583071892310e31334c8d09f0851a64f6fbd
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-20 16:51:14 +0200

    added doc

--- server/src/model/term/Term.ts
+++ server/src/model/term/Term.ts
@@ -1,7 +1,5 @@
-import Operator from "../operators/Operator";
-
 /**
- * TODO doc
+ * Represents a abstract node in an AST used to represent propositional formulas.
  *
  * @author Felix Strobel <https://github.com/felixstrobel>
  * @author Max Lohrmann <https://github.com/Max0440>
@@ -10,11 +8,23 @@ export default abstract class Term {
 	protected leftChild: Term | null;
 	protected rightChild: Term | null;
 
+	/**
+	 * Constructs a {@link Term}.
+	 *
+	 * @param leftChild The left child of the AST.
+	 * @param rightChild The right child of the AST.
+	 */
 	constructor(leftChild: Term | null, rightChild: Term | null) {
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
 	}
 
+	/**
+	 * Evaluates the term recursively based on the given variable assignment.
+	 *
+	 * @param variableAssignment The variable assignment.
+	 * @returns The evaluation of the term.
+	 */
 	public abstract eval(variableAssignment: Map<string, boolean>): boolean;
 
 	public abstract toString(): string;
