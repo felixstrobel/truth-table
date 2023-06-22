1a47385b1afbae274654fd1c56c5c2ec6dddf278
Felix Strobel <main.felixstrobel@gmail.com>
2023-06-22 20:34:09 +0200

    Further improving

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,56 +1,63 @@
-"use client";
+'use client'
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import {Box, Button, Flex, Grid, IconButton, Input, InputGroup, InputRightElement} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Flex,
+    Grid,
+    IconButton,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Select,
+    Stack
+} from "@chakra-ui/react";
 import {CopyIcon} from "@chakra-ui/icons";
 import {useState} from "react";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-	const [value, setValue] = useState<string>("");
-	const quickButtons = ["¬", "∧", "⊼", "∨", "⊽", "→", "↔", "↮", "(", ")", ",", "A", "B", "C"];
+    const [value, setValue] = useState<string>("");
+    const quickButtons = ["¬", "∧", "⊼", "∨", "⊽", "→", "↔", "↮", "(", ")", ",", "A", "B", "C"];
 
-	const copyInputToClipBoard = () => {
-		navigator.clipboard.writeText(value).catch((e) => console.log(e));
-	};
+    const copyInputToClipBoard = () => {
+        navigator.clipboard.writeText(value).catch(e => console.log(e));
+    }
 
-	return (
-		<div className="w-screen min-h-screen overflow-hidden bg-gray-900">
-			<Flex wrap={"wrap"} minHeight={"100vh"} flexDirection={"column"}>
-				<Header />
+    return (
+        <div className="w-screen min-h-screen overflow-hidden bg-gray-900">
 
-				<Grid placeItems={"center"} flexGrow={1}>
-					<Flex w={"50%"} flexDir={"column"}>
-						<InputGroup size={"lg"} mb={4}>
-							<Input
-								type={"text"}
-								value={value}
-								onChange={(e) => setValue(e.target.value)}
-								placeholder={"Enter Boolean Expression ..."}
-							/>
-							<InputRightElement>
-								<IconButton
-									onClick={copyInputToClipBoard}
-									icon={<CopyIcon />}
-									aria-label={"Copy expression to clipboard"}
-								/>
-							</InputRightElement>
-						</InputGroup>
+            <Header/>
 
-						<Flex flexDir={"row"} flexWrap={"wrap"} justifyContent={"space-between"}>
-							{quickButtons.map((button) => {
-								return (
-									<Button key={button} onClick={() => setValue(value + button)}>
-										{button}
-									</Button>
-								);
-							})}
-						</Flex>
-					</Flex>
-					<Box>{/*TODO Cheatsheat*/}</Box>
-				</Grid>
+            <Grid placeItems={"center"}>
+                <Stack w={"50%"}>
+                    <InputGroup size={"lg"} mb={4}>
+                        <Input
+                            type={"text"}
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                            placeholder={"Enter Boolean Expression ..."}
+                        />
+                        <InputRightElement>
+                            <IconButton onClick={copyInputToClipBoard} icon={<CopyIcon/>}
+                                        aria-label={"Copy expression to clipboard"}/>
+                        </InputRightElement>
+                    </InputGroup>
 
-				<Footer />
-			</Flex>
-		</div>
-	);
+                    {/*TODO fix flex wrap*/}
+                    <Flex flexDir={"row"} flexWrap={"wrap"} justifyContent={"space-between"}>
+                        {quickButtons.map(button => {
+                            return <Button key={button} onClick={() => setValue(value + button)}>{button}</Button>
+                        })}
+                        <Select variant='filled' placeholder='Filled'/>
+                    </Flex>
+                </Stack>
+                <Box>
+                    {/*TODO Cheatsheat*/}
+                </Box>
+            </Grid>
+
+            <Footer/>
+        </div>
+    );
 }
