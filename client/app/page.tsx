93a9860bb9c064d2145a4b69909af6088479c2f5
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-23 22:27:34 +0200

    removed not needed div

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 import Header from "@/components/Header";
-import {Flex} from "@chakra-ui/react";
+import {Flex, Box} from "@chakra-ui/react";
 import Footer from "@/components/Footer";
 import Insertion from "@/components/Insertion";
 
 export default function Home() {
-    return (
-        <div className="w-screen min-h-screen overflow-hidden bg-gray-900">
-            <Flex wrap={"wrap"} minHeight={"100vh"} flexDirection={"column"}>
-                <Header/>
+	return (
+		<Flex
+			width={"100vw"}
+			minH={"100vh"}
+			overflow={"hidden"}
+			backgroundColor={"#282a2e"}
+			wrap={"wrap"}
+			minHeight={"100vh"}
+			flexDirection={"column"}
+		>
+			<Header />
 
-                <Insertion/>
+			<Insertion />
 
-                <Footer/>
-            </Flex>
-        </div>
-    );
+			<Footer />
+		</Flex>
+	);
 }
