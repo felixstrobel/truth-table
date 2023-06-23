b6ffb4afa01314e09f339f4712fef435821592c3
Max Lohrmann <mlohrmann11@gmail.com>
2023-06-23 16:47:48 +0200

    changed table appearance

--- client/components/TableRepresentation.tsx
+++ client/components/TableRepresentation.tsx
@@ -1,36 +1,43 @@
 import {Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
 
 export default function TableRepresentation() {
+	// TODO: get data from parser
+	const data = [
+		{A: false, B: false, C: false, "(A∧B)∨C": false},
+		{A: false, B: false, C: true, "(A∧B)∨C": true},
+		{A: false, B: true, C: false, "(A∧B)∨C": false},
+		{A: false, B: true, C: true, "(A∧B)∨C": true},
+		{A: true, B: false, C: false, "(A∧B)∨C": false},
+		{A: true, B: false, C: true, "(A∧B)∨C": true},
+		{A: true, B: true, C: false, "(A∧B)∨C": true},
+		{A: true, B: true, C: true, "(A∧B)∨C": true},
+	];
 
-    // TODO: get data from parser
-    const data = [
-        {A: false, B: false, C: false, '=': false},
-        {A: false, B: false, C: true, '=': true},
-        {A: false, B: true, C: false, '=': false},
-        {A: false, B: true, C: true, '=': true},
-        {A: true, B: false, C: false, '=': false},
-        {A: true, B: false, C: true, '=': true},
-        {A: true, B: true, C: false, '=': true},
-        {A: true, B: true, C: true, '=': true}
-    ];
-
-    // TODO: fix keys
-    return <TableContainer mx={"auto"} w={"80%"} display={"grid"} placeItems={"center"} fontSize={"lg"}>
-        <Table variant='simple'>
-            <Thead>
-                <Tr>
-                    {Object.entries(data[0]).map(column => <Th fontSize={"xl"} fontWeight={"extrabold"}
-                                                               key={column[0]}>{column[0]}</Th>)}
-                </Tr>
-            </Thead>
-            <Tbody>
-                {data.map(evaluation =>
-                    <Tr key={evaluation}>
-                        {Object.entries(evaluation).map(column => <Td>{column[1] ? "1" : "0"}</Td>
-                        )}
-                    </Tr>
-                )}
-            </Tbody>
-        </Table>
-    </TableContainer>
+	// TODO: fix keys
+	return (
+		<TableContainer mx={"auto"} maxW={"90%"} display={"grid"} placeItems={"center"} fontSize={"lg"}>
+			<Table variant="simple">
+				<Thead>
+					<Tr>
+						{Object.entries(data[0]).map((column) => (
+							<Th key={column[0]} fontSize={"xl"} fontWeight={"extrabold"} textAlign={"center"}>
+								{column[0]}
+							</Th>
+						))}
+					</Tr>
+				</Thead>
+				<Tbody>
+					{data.map((evaluation, index) => (
+						<Tr key={index}>
+							{Object.entries(evaluation).map((column, index) => (
+								<Td key={index} textAlign={"center"}>
+									{column[1] ? "T" : "F"}
+								</Td>
+							))}
+						</Tr>
+					))}
+				</Tbody>
+			</Table>
+		</TableContainer>
+	);
 }
