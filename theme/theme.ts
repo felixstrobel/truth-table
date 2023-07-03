9da92c32f0a225108f7e14c4771edc812b26df65
Max Lohrmann <mlohrmann11@gmail.com>
2023-07-03 21:07:08 +0200

    added fallback fonts

--- theme/theme.ts
+++ theme/theme.ts
@@ -26,6 +26,15 @@ const colors = {
 const comfortaa = Comfortaa({
     subsets: ["latin-ext"],
     display: "swap",
+    fallback: [
+        "Arial",
+        "Helvetica Neue",
+        "Roboto",
+        "sans-serif",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+    ],
 });
 
 export default extendTheme({
