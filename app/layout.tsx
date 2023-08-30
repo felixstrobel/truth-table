d424ce46ad1687f9d12afa661d2a6ecd4fe0c577
Max Lohrmann <mlohrmann11@gmail.com>
2023-08-30 19:23:03 +0200

    added icons & og/fb tags to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,37 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
     return (
         <html lang="en">
-            {/* TODO: Facebook & Twitter tags */}
             <head>
                 <link rel="manifest" href="/manifest.json" />
-
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-                <meta name="msapplication-TileColor" content="#171717" />
                 <meta name="theme-color" content="#171717" />
+
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="256x256" href="/icon-256x256.png" />
+                <link rel="icon" type="image/png" sizes="384x384" href="/icon-384x384.png" />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/android-chrome-192x192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="512x512"
+                    href="/android-chrome-512x512.png"
+                />
+
+                <meta name="msapplication-TileColor" content="#171717" />
+
+                <meta property="og:image" content="/android-chrome-512x512.png" />
+                <meta name="twitter:image" content="/android-chrome-512x512.png" />
+
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             </head>
             <body>
                 <ColorModeScript initialColorMode={theme.config.initialColorMode} />
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,19 @@ export const metadata: Metadata = {
     title: "Truth Table Generator",
     description:
         "Rapidly convert all your Boolean expressions into a nice-looking (dark-themed) truth table",
+    openGraph: {
+        type: "website",
+        title: "Truth Table Generator",
+        description:
+            "Rapidly convert all your Boolean expressions into a nice-looking (dark-themed) truth table",
+        url: "https://truth-table.com",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Truth Table Generator",
+        description:
+            "Rapidly convert all your Boolean expressions into a nice-looking (dark-themed) truth table",
+    },
 };
 
 const Main = () => {
