d673e301085de583fb312500f98f6050dc965e08
Felix Strobel <main.felixstrobel@gmail.com>
2023-10-15 12:50:06 +0200

    Restyled the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,21 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: "Not Found",
+};
+
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <div className="max-w-3xl text-lg">
+            Oops! It seems you've reached a page that doesn't exist. Please check the URL or
+            navigate back to our{" "}
+            <Link className="italic" href={"/"}>
+                homepage
+            </Link>{" "}
+            to find what you're looking for.
+        </div>
+    );
 };
 
 export default NotFound;
