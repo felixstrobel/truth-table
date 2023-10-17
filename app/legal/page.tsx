fa6d1bda15f0d8f26248f2cf6cd8b0edb2f93c5c
Max Lohrmann <mlohrmann11@gmail.com>
2023-10-17 21:41:57 +0200

    changed border color in light mode

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 const Page = () => {
     return (
         <div className="flex flex-col lg:flex-row flex-wrap gap-x-0 lg:gap-x-16 gap-y-8 ">
-            <aside className="basis-0 flex-grow flex-shrink p-6 leading-8 rounded-lg shadow dark:bg-neutral-800/80 border dark:border-neutral-700 dark:hover:bg-neutral-800">
+            <aside className="basis-0 flex-grow flex-shrink p-6 leading-8 rounded-lg shadow dark:bg-neutral-800/80 border border-neutral-400 dark:border-neutral-700 dark:hover:bg-neutral-800">
                 <h1 className="font-bold text-2xl">Impressum</h1>
                 <h2 className="mt-8 mb-2 font-bold text-lg">Angaben gemäß § 5 TMG</h2>
                 <p>Max Lohrmann</p>
@@ -32,7 +32,7 @@ const Page = () => {
                     E-Mail: mail@truth-table.com
                 </Link>
             </aside>
-            <aside className="basis-0 flex-grow flex-shrink p-6 leading-8 rounded-lg shadow dark:bg-neutral-800/80 border dark:border-neutral-700 dark:hover:bg-neutral-800">
+            <aside className="basis-0 flex-grow flex-shrink p-6 leading-8 rounded-lg shadow dark:bg-neutral-800/80 border border-neutral-400 dark:border-neutral-700 dark:hover:bg-neutral-800">
                 <h1 className="font-bold text-2xl">Legal Notice</h1>
                 <h2 className="mt-8 mb-2 font-bold text-lg">
                     This Legal Notice complies with the German laws under § 5 TMG and § 55 RStV
--- components/layout/ScrollToTopButton.tsx
+++ components/layout/ScrollToTopButton.tsx
@@ -25,7 +25,7 @@ const ScrollToTopButton = () => {
         <button
             onClick={scrollToTop}
             className={clsx(
-                "fixed bottom-4 right-3 grid place-items-center w-10 h-10 rounded-lg border dark:bg-neutral-700/40 dark:hover:bg-neutral-700/80 dark:border-neutral-600 dark:focus:border-violet-500 dark:placeholder-neutral-300 dark:focus:ring-violet-600",
+                "fixed bottom-4 right-3 grid place-items-center w-10 h-10 rounded-lg border dark:bg-neutral-700/40 dark:hover:bg-neutral-700/80 border-neutral-400 dark:border-neutral-600 dark:focus:border-violet-500 dark:placeholder-neutral-300 dark:focus:ring-violet-600",
                 !visible && "hidden"
             )}
         >
